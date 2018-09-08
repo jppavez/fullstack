be4a722a62725302c7ff704ffc5cc3c27033b26d
Juan Pablo Pavez <jp@jpavezf.net>
2018-09-07 23:27:29 -0300

    Add views for book detail, category detail, index

--- techk/apps/base/views.py
+++ techk/apps/base/views.py
@@ -1,5 +1,50 @@
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import redirect
+from django.template import loader
+from .models.Category import Category
+from .models.Book import Book
 
 
 def index(request):
-    return HttpResponse('Hello, world!')
+    categories = Category.objects.order_by('name').all()
+
+    template = loader.get_template('index.html')
+
+    context = {
+        'categories': categories
+    }
+
+    return HttpResponse(template.render(context, request))
+
+
+def category_detail(request, category_id):
+
+    try:
+        category = Category.objects.get(id=category_id)
+    except Category.DoesNotExist:
+        raise Http404
+
+    template = loader.get_template('category.html')
+
+    context = {
+        'category': category
+    }
+
+    return HttpResponse(template.render(context, request))
+
+
+def book_detail(request, book_id):
+
+    try:
+        book = Book.objects.get(id=book_id)
+    except Book.DoesNotExist:
+        raise Http404
+
+    template = loader.get_template('book.html')
+
+    context = {
+        'book': book
+    }
+
+    return HttpResponse(template.render(context, request))
