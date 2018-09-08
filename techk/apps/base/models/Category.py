fc52c287480e600fb6f0f59f1771f589fe11a146
Juan Pablo Pavez <juanpablo@finvox.com>
2018-09-07 21:07:47 -0300

    Add Category create method

--- techk/apps/base/models/Category.py
+++ techk/apps/base/models/Category.py
@@ -6,3 +6,9 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
 
     books = models.ManyToManyField(Book, related_name='categories')
+
+    @staticmethod
+    def create(name):
+        category = Category.objects.create(name=name)
+
+        return category
